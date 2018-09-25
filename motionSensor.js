12aad9d562c2687f9954c360afb7f1724558a188
Thomas Clukies <thomas.clukies@gmail.com>
2018-09-25 11:02:59 -0600

    working again with pin mode set to in and in in code

--- motionSensor.js
+++ motionSensor.js
@@ -1,45 +1,3 @@
-// const Gpio = require('onoff').Gpio;
-// const led = new Gpio(13, 'out'); // gpio 4 as out
-// const led2 = new Gpio(19, 'out'); // gpio 4 as out
-// const led3 = new Gpio(18, 'out'); // gpio 4 as out
-// const led4 = new Gpio(16, 'out'); // gpio 4 as out
-
-
-// //   socket.emit('chat message', $('#m').val());
-
-// // function lock(){
-// //     socket.emit("lock", 99)
-// // }
-
-// // lock()
-// let currentValue = led2.readSync();
-// console.log("currentValue" + currentValue)
-
-// let lastValue = led2.readSync();
-// console.log("lastValue" + lastValue)
-
-
-// iv = setInterval(function() {
-// currentValue = led2.readSync();
-// if (currentValue != lastValue){
-//     console.log("tampered!!!!!!")
-//     console.log(currentValue + " " +lastValue)
-//     lastValue = currentValue
-
-// }
-// }, 200);
-
-// iv2 = setInterval(function() {
-   
-//     console.log(led.readSync())
-//     console.log(led2.readSync())
-//     console.log(led3.readSync())
-//     console.log(led4.readSync())
-//     console.log("_________")
-
-//     }, 200);
-
-
 const Gpio = require('onoff').Gpio;
 const led = new Gpio(13, 'in'); // gpio 4 as in
 const led2 = new Gpio(19, 'in'); // gpio 4 as in
@@ -54,12 +12,30 @@ const led4 = new Gpio(16, 'in'); // gpio 4 as in
 // }
 
 // lock()
+let currentValue = led2.readSync();
+console.log("currentValue" + currentValue)
+
+let lastValue = led2.readSync();
+console.log("lastValue" + lastValue)
+
 
 iv = setInterval(function() {
-    console.log((led.readSync()*100))
-    console.log((led2.readSync()*100))
-    console.log((led3.readSync()*100))
-    console.log((led4.readSync()*100))
-    console.log(("---"))
+currentValue = led2.readSync();
+if (currentValue != lastValue){
+    console.log("tampered!!!!!!")
+    console.log(currentValue + " " +lastValue)
+    lastValue = currentValue
 
+}
 }, 200);
+
+iv2 = setInterval(function() {
+   
+    console.log(led.readSync())
+    console.log(led2.readSync())
+    console.log(led3.readSync())
+    console.log(led4.readSync())
+    console.log("_________")
+
+    }, 200);
+
