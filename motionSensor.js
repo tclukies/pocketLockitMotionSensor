7d23c1b409a8b64f00d8b9e5ec27902adc3e736f
Thomas Clukies <thomas.clukies@gmail.com>
2018-09-24 18:14:15 -0600

    getting files mixed up i think this is right

--- motionSensor.js
+++ motionSensor.js
@@ -1,8 +1,6 @@
-var io= require("socket.io-client")
 const Gpio = require('onoff').Gpio;
-const LED = new Gpio(4, 'out'); // gpio 4 as out
+const led = new Gpio(13, 'out'); // gpio 4 as out
 
-var socket = io(process.env.SOCKET_HOST || "http://localhost:3000");
 //   socket.emit('chat message', $('#m').val());
 
 // function lock(){
@@ -11,16 +9,6 @@ var socket = io(process.env.SOCKET_HOST || "http://localhost:3000");
 
 // lock()
 
-socket.on("unlock now", onUnlock)
-
-function onUnlock(id){
-    console.log("onUnlock", id)
-    LED.writeSync(0);
-}
-
-socket.on("lock now", onLock)
-
-function onLock(id){
-    console.log("onLock", id)
-    LED.writeSync(1)
-}
+iv = setInterval(function() {
+    console.log((led.readSync()))
+}, 200);
