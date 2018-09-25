eada68b711e25ef9fd780b7f75813fe1b4393533
Thomas Clukies <thomas.clukies@gmail.com>
2018-09-24 18:21:34 -0600

    trying a bunch of pins

--- motionSensor.js
+++ motionSensor.js
@@ -1,5 +1,9 @@
 const Gpio = require('onoff').Gpio;
 const led = new Gpio(13, 'out'); // gpio 4 as out
+const led2 = new Gpio(19, 'out'); // gpio 4 as out
+const led3 = new Gpio(18, 'out'); // gpio 4 as out
+const led4 = new Gpio(16, 'out'); // gpio 4 as out
+
 
 //   socket.emit('chat message', $('#m').val());
 
@@ -11,4 +15,9 @@ const led = new Gpio(13, 'out'); // gpio 4 as out
 
 iv = setInterval(function() {
     console.log((led.readSync()))
+    console.log((led2.readSync()))
+    console.log((led3.readSync()))
+    console.log((led4.readSync()))
+    console.log(("---"))
+
 }, 200);
